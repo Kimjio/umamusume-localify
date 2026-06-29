cee917d522fe4f26f9a424b654c0f5dc7f05c96e
Ji O <kimjioh0927@gmail.com>
2026-06-29 13:16:53 +0900

    Add Traditional Chinese settings text

--- src/settings_text.hpp
+++ src/settings_text.hpp
@@ -15,6 +15,8 @@ namespace LocalifySettings
 	{
 		bool isKor = Game::CurrentGameRegion == Game::Region::KOR || config::localify_settings_language == IL2CPP_STRING("ko");
 		bool isEng = Game::CurrentGameRegion == Game::Region::ENG || config::localify_settings_language == IL2CPP_STRING("en");
+		bool isZhHant = Game::CurrentGameRegion == Game::Region::TWN || config::localify_settings_language == IL2CPP_STRING("zh-TW") ||
+			config::localify_settings_language == IL2CPP_STRING("zh-Hant") || config::localify_settings_language == IL2CPP_STRING("zh-Hant-TW");
 
 		if (id == "settings_title")
 		{
@@ -25,6 +27,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Localify Settings");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("Localify 設定");
 			}
 			return IL2CPP_STRING("Localify設定");
 		}
@@ -39,6 +45,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Open settings");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("開啟設定");
+			}
 			return IL2CPP_STRING("設定を開く");
 		}
 
@@ -51,6 +61,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Setting are not enabled");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("設定未啟用");
 			}
 			return IL2CPP_STRING("設定が有効になっていません");
 		}
@@ -65,6 +79,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("<color=#ff911c>Changes will be applied after restart.</color>");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("<color=#ff911c>變更將在重新啟動遊戲後生效</color>");
+			}
 			return IL2CPP_STRING("<color=#ff911c>変更はゲームの再実行後に反映されます</color>");
 		}
 
@@ -77,6 +95,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Anti-aliasing");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("抗鋸齒");
 			}
 			return IL2CPP_STRING("アンチエイリアシング");
 		}
@@ -91,6 +113,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Graphics quality");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("圖形品質");
+			}
 			return IL2CPP_STRING("グラフィックの品質");
 		}
 
@@ -103,6 +129,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return  IL2CPP_STRING("Character dialogue caption");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("角色對話字幕");
 			}
 			return IL2CPP_STRING("キャラクターのダイアログテキストをキャプションで表示");
 		}
@@ -117,6 +147,10 @@ namespace LocalifySettings
 			{
 				return  IL2CPP_STRING("Character dialogue caption");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("將角色對話文字\n以字幕顯示");
+			}
 			return IL2CPP_STRING("キャラクターのダイアログテキストを\nキャプションで表示");
 		}
 
@@ -129,6 +163,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Characters per line");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("每行字數");
 			}
 			return IL2CPP_STRING("一行あたりの文字数");
 		}
@@ -143,6 +181,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Font size");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("字體大小");
+			}
 			return IL2CPP_STRING("フォントサイズ");
 		}
 
@@ -155,6 +197,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("X-coordinate");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("X座標");
 			}
 			return IL2CPP_STRING("X座標");
 		}
@@ -169,6 +215,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Y-coordinate");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("Y座標");
+			}
 			return IL2CPP_STRING("Y座標");
 		}
 
@@ -181,6 +231,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Background transparency");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("背景透明度");
 			}
 			return IL2CPP_STRING("背景透明度");
 		}
@@ -195,6 +249,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Font color");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("字體顏色");
+			}
 			return IL2CPP_STRING("フォントの色");
 		}
 
@@ -207,6 +265,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Outline size");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("描邊大小");
 			}
 			return IL2CPP_STRING("アウトラインサイズ");
 		}
@@ -221,6 +283,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Outline color");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("描邊顏色");
+			}
 			return IL2CPP_STRING("アウトラインの色");
 		}
 
@@ -233,6 +299,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Show caption");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("顯示字幕");
 			}
 			return IL2CPP_STRING("字幕表示");
 		}
@@ -247,6 +317,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Show notification");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("顯示通知");
+			}
 			return IL2CPP_STRING("通知表示");
 		}
 
@@ -259,6 +333,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Sample caption");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("範例字幕");
 			}
 			return IL2CPP_STRING("サンプル字幕");
 		}
@@ -273,6 +351,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Ms. VICTORIA Champions Meeting Standings");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("Ms. VICTORIA 冠軍會議的\n排名顯示");
+			}
 			return IL2CPP_STRING("Ms. VICTORIAチャンピオンズミーティングの\n順位表示");
 		}
 
@@ -285,6 +367,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Champions Meeting Schedule");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("冠軍會議行程");
 			}
 			return IL2CPP_STRING("チャンピオンズミーティングスケジュール");
 		}
@@ -299,6 +385,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Champions Meeting year");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("冠軍會議年份");
+			}
 			return IL2CPP_STRING("チャンピオンズミーティングの年");
 		}
 
@@ -311,6 +401,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Dump MessagePack");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("MessagePack 轉存");
 			}
 			return IL2CPP_STRING("MessagePackダンプ");
 		}
@@ -325,6 +419,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Dump MessagePack request");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("MessagePack 請求轉存");
+			}
 			return IL2CPP_STRING("MessagePackリクエストダンプ");
 		}
 
@@ -337,6 +435,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Unlock full concert theater");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("解鎖整個演唱會劇場");
 			}
 			return IL2CPP_STRING("ライブシアター全体解放");
 		}
@@ -351,6 +453,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Always show playback slider");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("始終顯示播放滑桿");
+			}
 			return IL2CPP_STRING("プレイスライダーを常に表示");
 		}
 
@@ -363,6 +469,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Repeat playback");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("重複播放");
 			}
 			return IL2CPP_STRING("ループプレイ");
 		}
@@ -377,6 +487,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Changes the availability of unreleased/unreleased/unpublished songs and Umamusume and dresses.");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("將未解禁/未解放/未公開的曲目，以及 Umamusume 與服裝變為可使用。");
+			}
 			return IL2CPP_STRING("未解禁/未解放/未公開曲及びウマ娘とドレスを使えるように変更します。");
 		}
 
@@ -389,6 +503,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Allow deletion of WebView cookies");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("允許刪除 WebView Cookie");
 			}
 			return IL2CPP_STRING("WebView Cookieの削除を許可する");
 		}
@@ -403,6 +521,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Clear WebView cache");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("清除 WebView 快取");
+			}
 			return IL2CPP_STRING("WebViewキャッシュの削除");
 		}
 
@@ -416,6 +538,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Do you want to clear the WebView cache?");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("確定要清除 WebView 快取嗎？");
+			}
 			return IL2CPP_STRING("WebViewキャッシュを削除しますか？");
 		}
 
@@ -428,6 +554,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Deleted");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("已刪除");
 			}
 			return IL2CPP_STRING("削除しました");
 		}
@@ -447,6 +577,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("CySpring update mode");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("CySpring 更新模式");
+			}
 			return IL2CPP_STRING("CySpringアップデートモード");
 		}
 
@@ -459,6 +593,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Disable CySpring native library");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("停用 CySpring 原生函式庫");
 			}
 			return IL2CPP_STRING("CySpringネイティブライブラリの無効化");
 		}
@@ -473,6 +611,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Remove CySpring Mono frame scale limit");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("取消 CySpring Mono 幀縮放限制");
+			}
 			return IL2CPP_STRING("CySpring Monoフレームスケール制限の解除");
 		}
 
@@ -485,6 +627,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Turn Disable CySpring native library ON to use");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("將 Disable CySpring 原生庫設為 ON 後可使用");
 			}
 			return IL2CPP_STRING("CySpringネイティブライブラリのONにすると、使用できます");
 		}
@@ -499,6 +645,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Change the frame scale based on the current FPS settings.\nDepending on the FPS settings, the physical effects may be excessively applied.");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("根據當前 FPS 設定更改幀縮放。\n根據 FPS 設定，物理效果可能會過度作用。");
+			}
 			return IL2CPP_STRING("フレームスケールを現在のFPS設定に基づいて変更します。\nFPS設定によっては物理効果が過度に作用する可能性があります。");
 		}
 
@@ -511,6 +661,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("CySpring legacy behavior");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("CySpring 傳統行為");
 			}
 			return IL2CPP_STRING("CySpringレガシー動作");
 		}
@@ -525,6 +679,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Turn Disable CySpring native library OFF to use");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("將 Disable CySpring 原生庫設為 OFF 後可使用");
+			}
 			return IL2CPP_STRING("CySpringネイティブライブラリのOFFにすると、使用できます");
 		}
 
@@ -537,6 +695,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Air Resistance");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("空氣阻力");
 			}
 			return IL2CPP_STRING("空気抵抗");
 		}
@@ -551,6 +713,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Stiffness Force Rate");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("形狀恢復力率");
+			}
 			return IL2CPP_STRING("形状復元力レート");
 		}
 
@@ -563,6 +729,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Movement Inertia");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("移動慣性");
 			}
 			return IL2CPP_STRING("移動慣性");
 		}
@@ -577,6 +747,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Motion Inertia");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("動作慣性");
+			}
 			return IL2CPP_STRING("モーション慣性");
 		}
 
@@ -589,6 +763,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Graphics");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("圖形");
 			}
 			return IL2CPP_STRING("グラフィック");
 		}
@@ -603,6 +781,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("UI animation scale");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("UI 動畫縮放");
+			}
 			return IL2CPP_STRING("UIアニメーションスケール");
 		}
 
@@ -615,6 +797,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("3D rendering resolution scale");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("3D 渲染解析度縮放");
 			}
 			return IL2CPP_STRING("3Dレンダリングの解像度スケール");
 		}
@@ -629,6 +815,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Screen");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("畫面");
+			}
 			return IL2CPP_STRING("画面");
 		}
 
@@ -641,6 +831,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Unlock resolution");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("解除解析度鎖定");
 			}
 			return IL2CPP_STRING("解像度固定解除");
 		}
@@ -655,6 +849,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("UI scale");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("UI 縮放");
+			}
 			return IL2CPP_STRING("UIスケール");
 		}
 
@@ -667,6 +865,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Free-form window");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("自由格式視窗");
 			}
 			return IL2CPP_STRING("自由形式ウィンドウ");
 		}
@@ -681,6 +883,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("UI scale (Vertical)");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("UI 縮放（縱向）");
+			}
 			return IL2CPP_STRING("UIスケール(縦)");
 		}
 
@@ -693,6 +899,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("UI scale (Horizontal)");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("UI 縮放（橫向）");
 			}
 			return IL2CPP_STRING("UIスケール(横)");
 		}
@@ -707,6 +917,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Use of Third-Party notices");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("使用第三方公告");
+			}
 			return IL2CPP_STRING("サードパーティのお知らせの使用");
 		}
 
@@ -719,6 +933,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Use third-party notices instead of in-app notices");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("使用第三方公告取代應用內公告");
 			}
 			return IL2CPP_STRING("アプリ内お知らせの代わりにサードパーティのお知らせを使用します");
 		}
@@ -733,6 +951,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Taskbar");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("工作列");
+			}
 			return IL2CPP_STRING("タスクバー");
 		}
 
@@ -745,6 +967,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Show download progress");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("顯示下載進度");
 			}
 			return IL2CPP_STRING("ダウンロードの進行状況を表示");
 		}
@@ -759,6 +985,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Show Connecting progress");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("顯示連線進度");
+			}
 			return IL2CPP_STRING("Connectingの進捗状況を表示");
 		}
 
@@ -771,6 +1001,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Local data path");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("本地資料路徑");
 			}
 			return IL2CPP_STRING("ローカルデータパス");
 		}
@@ -790,6 +1024,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("You are responsible for any limitations, errors, or other issues that may arise from using this improvement mod.\n\nPlease check <color=#ff911c><i>");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("使用本改進模組所產生的使用限制、進行錯誤或其他問題由使用者自行承擔。\n\n繼續請勾選 <color=#ff911c><i>");
+			}
 			return IL2CPP_STRING("本改善モードを使用することによって発生する利用制限、進行エラーまたは\nその他の問題に対する責任は本人にあります。\n\n続行するには、<color=#ff911c><i>");
 		}
 
@@ -802,6 +1040,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("</i></color> to continue.");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("</i></color> 以繼續。");
 			}
 			return IL2CPP_STRING("</i></color>をチェックしてください。");
 		}
@@ -816,6 +1058,10 @@ namespace LocalifySettings
 			{
 				return IL2CPP_STRING("Umamusume");
 			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("賽馬娘");
+			}
 			return IL2CPP_STRING("ウマ娘");
 		}
 
@@ -828,6 +1074,10 @@ namespace LocalifySettings
 			if (isEng)
 			{
 				return IL2CPP_STRING("Unknown");
+			}
+			if (isZhHant)
+			{
+				return IL2CPP_STRING("未知");
 			}
 			return IL2CPP_STRING("不明");
 		}
